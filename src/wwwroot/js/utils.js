339888ad7849341473abf1f2805f2e47b6e9c5d6
ethanperry <ethanperry72@gmail.com>
2025-08-18 19:01:41 -0600

    Improve blog functionality
    - Add sidebar navigation to view posts sorted by year published.
    - Define ContentTree pseudo-component to easily style lists
    - Improve CSS custom variables.

--- src/wwwroot/js/utils.js
+++ src/wwwroot/js/utils.js
@@ -3,5 +3,20 @@ export function formatDate(date) {
     const year = d.getFullYear();
     const month = String(d.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
     const day = String(d.getDate()).padStart(2, '0');
-    return `${year} ${month} ${day}`;
+    return `${year}.${month}.${day}`;
+}
+
+export function getPostsByYear(posts) {
+    // Sort posts newest first
+    const sorted = posts.sort((a, b) => new Date(b.data.date) - new Date(a.data.date));
+
+    const postsByYear = {};
+
+    for (const post of sorted) {
+        const year = new Date(post.data.date).getFullYear();
+        if (!postsByYear[year]) postsByYear[year] = [];
+        postsByYear[year].push(post);
+    }
+
+    return postsByYear;
 }
