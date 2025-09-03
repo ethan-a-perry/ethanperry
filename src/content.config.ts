b135b663ed37bedf895beb2822066b2bc9a841a6
ethanperry <ethanperry72@gmail.com>
2025-09-03 17:53:40 -0600

    Migrate Projects page to Astro content collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,7 +6,7 @@ import { z, defineCollection } from "astro:content";
 
 // Define a `loader` and `schema` for each collection
 const writings = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/writings" }),
+    loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/writings" }),
     schema: z.object({
         title: z.string(),
         date: z.date(),
@@ -17,5 +17,15 @@ const writings = defineCollection({
     })
 });
 
+const projects = defineCollection({
+    loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/content/projects",  }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        draft: z.boolean().default(false),
+        tags: z.array(z.string())
+    })
+});
+
 // Export a single `collections` object to register your collection(s)
-export const collections = { writings };
+export const collections = { writings, projects };
