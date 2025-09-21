f6c7c9cdac9e7b56064d999f0a66e1e08e7108e1
ethanperry <ethanperry72@gmail.com>
2025-09-21 10:22:20 -0600

    Configure @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import remarkCodeBlockWrapper from "./src/plugins/remark-code-block-wrapper.js";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
+    site: 'https://ethanperry.net',
+    integrations: [mdx(), sitemap()],
     markdown: {
         remarkPlugins: [remarkCodeBlockWrapper],
         shikiConfig: {
