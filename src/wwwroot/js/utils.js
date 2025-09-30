d1890623eb7862f1b6ed8c2d2c97a517d6ff19e8
ethanperry <ethanperry72@gmail.com>
2025-09-29 18:29:48 -0600

    Create WritingsIndex and WorkIndex components
    - Sort content collection by date (most recent first).
    - Add optional `limit` prop.
    - Add scale effect on images when hovered.

--- src/wwwroot/js/utils.js
+++ src/wwwroot/js/utils.js
@@ -11,3 +11,11 @@ export function formatDateYYYYMMDD(date) {
     if (!date) return;
     return date.toISOString().split('T')[0];
 }
+
+export function sortCollectionByMostRecent(collection) {
+    // return collection.sort((a, b) =>
+    //     new Date(b.data.datePublished).getTime() - new Date(a.data.datePublished).getTime()
+    // );
+
+    return collection.sort((a, b) => Date.parse(b.data.datePublished) - Date.parse(a.data.datePublished));
+}
