20d9281816b85968f598db6598070f7921ab984e
ethanperry <ethanperry72@gmail.com>
2026-01-10 09:59:28 -0700

    Improve Markdoc config and remove whitespace from links in .mdoc files

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -1,50 +1,57 @@
-import { defineMarkdocConfig, nodes, component } from '@astrojs/markdoc/config';
-import shiki from '@astrojs/markdoc/shiki';
+import { defineMarkdocConfig, nodes, component } from "@astrojs/markdoc/config";
+import shiki from "@astrojs/markdoc/shiki";
 
 export default defineMarkdocConfig({
-    nodes: {
-        document: {
-            ...nodes.document,
-            render: null,
-        },
-    },
-    extends: [
-        shiki({
-            theme: 'github-light'
-        }),
-    ],
-    tags: {
-        image: {
-            render: 'img',
-            attributes: {
-                src: { type: String, required: true },
-                alt: { type: String, required: true },
-                width: { type: Number },
-                height: { type: Number },
-            },
-        },
-        ExternalLink: {
-            render: component('./src/components/Link.astro'),
-            attributes: {
-            		href: { type: String, required: true },
-              	external: { type: Boolean, default: true }
-            },
-        },
-        CodeBlock: {
-            render: component('./src/components/CodeBlock.astro'),
-            attributes: {
-                language: { type: String },
-                filename: { type: String }
-            }
-        },
+	nodes: {
+		document: {
+			...nodes.document,
+			render: null,
+		},
+		link: {
+			...nodes.link,
+			render: component("./src/components/Link.astro"),
+			attributes: {
+				...nodes.link.attributes,
+			}
+		}
+	},
+	extends: [
+		shiki({
+			theme: "github-light",
+		}),
+	],
+	tags: {
+		image: {
+			render: "img",
+			attributes: {
+				src: { type: String, required: true },
+				alt: { type: String, required: true },
+				width: { type: Number },
+				height: { type: Number },
+			},
+		},
+		ExternalLink: {
+			render: component("./src/components/Link.astro"),
+			attributes: {
+				href: { type: String, required: true },
+				external: { type: Boolean, default: true },
+			},
+		},
+		CodeBlock: {
+			render: component("./src/components/CodeBlock.astro"),
+			attributes: {
+				language: { type: String },
+				filename: { type: String },
+			},
+		},
 		FullWidth: {
-      		render: 'div',
-        	attributes: {
-				class: { type: String, default: 'full-width' }
-         	},
+			render: "div",
+			attributes: {
+				class: { type: String, default: "full-width" },
+			},
 		},
 		Sudoku: {
-			render: component('./src/components/Sudoku.astro'),
-		}
-    },
+			render: component("./src/components/Sudoku.astro"),
+		},
+	},
 });
